95f4f209c40e6116c0c416bc6a96126d5303914f
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-04-21 20:49:54 -0500

    icons/space setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import Sidebar from './components/Sidebar';
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Gallery from './pages/Gallery';
+import Employment from "./pages/Employment";
 
 const App = () => {
   return (
-    <div>
-      <Sidebar/>
-    </div>
+    <BrowserRouter>
+    <Sidebar>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/gallery" element={<Gallery/>}/>
+        <Route path="/employment" element={<Employment/>}/>
+      </Routes>
+      </Sidebar>
+    </BrowserRouter>
   );
 };
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,52 @@
 import React from "react";
+import {MdOutlineCabin, MdOutlineMailOutline} from "react-icons/md";
+import { CgGirl } from "react-icons/cg";
+import { AiOutlinePicture } from "react-icons/ai";
+import { GrPaint } from "react-icons/gr";
+import { FaBars } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-    return (
-        <div>
-            <h1>Sidebar setup</h1>
+  const menuItem = [
+    {
+      path: "/",
+      name: "home",
+      icon: <MdOutlineCabin />,
+    },
+    {
+      path: "/about",
+      name: "About",
+      icon: <CgGirl />,
+    },
+    {
+      path: "/contact",
+      name: "Contact",
+      icon: <MdOutlineMailOutline />,
+    },
+    {
+      path: "/gallery",
+      name: "Gallery",
+      icon: <AiOutlinePicture />,
+    },
+    {
+      path: "/employment",
+      name: "Employment",
+      icon: <GrPaint />,
+    },
+  ];
+
+  return (
+    <div className="container">
+      <div className="sidebar">
+        <div className="top_section">
+          <h1 className="logo">Logo</h1>
+          <div className="bars">
+            <FaBars />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
