461057e0b90a39d27b5d6d1caf29f45babcf4aca
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-04-21 20:05:26 -0500

    pages and sidebar component setup

---
+++ src/pages/Employment.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Employment = () => {
+    return (
+        <div>
+            <h1>Work for Cabin Girl</h1>
+        </div>
+    )
+}
+
+export default Employment;
