916ac7368482776a4138a8819784ae9f26a1c19c
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-04-22 21:41:28 -0500

    sidebar working, need client opinion on ugly colors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.css';
 import Sidebar from "./components/Sidebar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
