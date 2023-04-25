99e4d4670027c0aa7183ab8d8a5cea8ce3922df6
wagnergabe <102180936+wagnergabe@users.noreply.github.com>
2023-04-24 23:25:07 -0500

    basic image working

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import cabin from '../assets/images/cabin-wi.jpg'
+
+
 
 const Gallery = () => {
+
+        
+
     return (
         <div>
             <h1>Gallery</h1>
+            <img src = {cabin} alt = 'cabin'></img>
         </div>
     )
 }
